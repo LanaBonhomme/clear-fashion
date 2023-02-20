3c5ec9e46328eeb07910f60ec5c381526e94652a
LanaBonhomme <bonhomme.lana@gmail.com>
2023-02-20 16:23:00 +0100

    Workshop3

--- server/sandbox_circlesportswear.js
+++ server/sandbox_circlesportswear.js
@@ -1,18 +1,39 @@
 /* eslint-disable no-console, no-process-exit */
 const circlesportswearbrand = require('./eshops/circlesportswearbrand');
 
-async function sandbox (eshop = 'https://shop.circlesportswear.com/collections/collection-femme') {
-  try {
-    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+const link = [
+  'https://shop.circlesportswear.com/collections/collection-femme'
+]
 
-    const products = await circlesportswearbrand.scrape(eshop);
-
-    console.log(products);
-    console.log('done');
+async function sandbox (eshop = undefined, number = -1) {
+  if(number == -1 && eshop == undefined)
+  {
+    var allProducts = [];
+    for(var i = 0; i < link.length; i++)
+    {
+      allProducts.push(...await sandbox(link[i], i));
+    }
+    const fs = require('fs');
+ 
+    let data = JSON.stringify(allProducts);
+    fs.writeFileSync('products.json', data);
+    console.log("Products in the json file: " + allProducts.length);
     process.exit(0);
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
+  }
+
+  else{
+    try {
+      console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+
+      const products = await circlesportswearbrand.scrape(eshop);
+
+      console.log(products);
+      console.log('done');
+      process.exit(0);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
   }
 }
 
